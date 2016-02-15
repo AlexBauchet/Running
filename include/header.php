d33811371cc6f4ca56ebda90083a468b36b4d314
PaulineS <po_se_87@hotmail.com>
2016-02-15 14:34:17 +0100

    include modal

--- include/header.php
+++ include/header.php
@@ -18,46 +18,7 @@
 				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#loginModal">Connection</button>
 
 
-<!-- Login Modal -->
-<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			
-			<div class="modal-body">
-				
-				<div>
-					<button type="button" class="btn btn-primary" data-dismiss="modal">Connexion avec Facebook</button>
-				</div>
 
-				<div>
-					<button type="button" class="btn btn-danger" data-dismiss="modal">Connexion avec Google</button>
-				</div>
-
-				<hr>
-
-				<div>
-					<div>
-						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-					</div>
-					
-					<div>
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-					</div>
-					
-					<div>
-						<button type="button" class="btn btn-success">Connexion</button>
-					</div>
-
-				</div>
-			</div>
-
-			<div class="modal-footer">
-				<p>Vous n'avez pas de compte ? <a href="register.php">Inscription</a></p>
-			</div>
-
-		</div>
-	</div>
-</div>
 
 
 				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#registerModal">Inscription</button>
--- index.php
+++ index.php
@@ -59,21 +59,17 @@
 		<hr>
 
 
+		<!-- INCLUDE MODALS SUR HOMEPAGE UNIQUEMENT -->
+		<?php include(__DIR__.'/modal/login.php'); ?>
+		<?php include(__DIR__.'/modal/register.php'); ?>
+
+
 		<!-- FOOTER -->
 		<?php include(__DIR__.'/include/footer.php'); ?>
 
 
 	</div> <!-- /container -->        
 
-	
-	<!-- SCRIPTS -->
-
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
-	<script src="js/vendor/bootstrap.min.js"></script>
-
-	<script src="js/main.js"></script>
 
 </body>
 </html>
