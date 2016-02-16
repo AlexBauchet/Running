456539b1a4c3319a189d5ec5da3f152e8b134139
PaulineS <po_se_87@hotmail.com>
2016-02-16 15:31:06 +0100

    reindent index.php

--- index.php
+++ index.php
@@ -6,22 +6,20 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Running</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Running</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <style>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 
-    </style>
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/modal.css">
-    <link rel="stylesheet" href="css/footer.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/modal.css">
+	<link rel="stylesheet" href="css/footer.css">
 
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 </head>
@@ -38,7 +36,7 @@
 
 	<!-- MAIN CONTENT -->
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->	
+	<!-- Main jumbotron for a primary marketing message or call to action -->	
 			<div id="slider">
 				<figure>
 					<img src="img/running.jpeg"></img>
