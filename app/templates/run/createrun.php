7f0e1ea2d447e7cfd6fa905fd7c6380772ae8eed
Dayday05 <deeckdavid@gmail.com>
2016-02-18 16:08:35 +0100

    modif createRun

--- app/templates/run/createrun.php
+++ app/templates/run/createrun.php
@@ -4,14 +4,16 @@
 
 	<!-- MAIN CONTENT -->
 	<div class="container">
-		<h1 class="text-center h1-seekRun">Créer une course</h1>
+		<h1 class="text-center h1-seekRun">Créer un Run</h1>
 	</div>
+
+	<br/>
 
 	<!-- Menu créer une course -->
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4 col-md-offset-4">
-				<form>
+			<div class="col-md-5">
+				<form class="form-horizontal">
 
 					<div class="form-group">
 						<label class="inputCreateRun" for="address">Adresse</label>
@@ -33,6 +35,15 @@
 						<input id="country" name="country" type="text" placeholder="Pays" class="form-control">
 					</div>
 
+				</form>
+			</div>
+		
+
+			<div class="col-md-2"></div>
+
+			<div class="col-md-5">
+				<form class="form-horizontal">
+
 					<div class="form-group">
 						<label class="inputCreateRun" for="departureDate">Date de départ</label>
 						<input id="departureDate" name="departureDate" type="date" placeholder="Date" class="form-control">
@@ -43,33 +54,35 @@
 						<input id="departureTime" name="departureTime" type="text" placeholder="Heure" class="form-control">
 					</div>
 
-					<div class="form-group">
-						<label class="inputCreateRun" for="runLevel">Niveau du run</label>
-						<input id="runLevel" name="runLevel" type="text" placeholder="Niveau" class="form-control">
-					</div>
-
 					<div class="form-group">				
 						<div id="rngs">
-						    <label class="inputCreateRun" for="level">Niveau de la course</label>
+						    <label class="inputCreateRun" for="level">Niveau du run</label>
 						    <input type="range" id="levelId" class="rng form-control" min="0" value="0" max="10" step="1">
 						    <label class="val">0</label>
 						</div>    
 					</div>
 
-					<a type="submit" class="btn btn-danger">Ajouter</a>
+					<div class="form-group">
+						<a type="submit" class="btn btn-danger">Ajouter</a>
+					</div>	
 
-				</form>
+				</form>	
 			</div>
 		</div>
-	</div>
-
-
+	</div>		
+	
+						
 	<!-- Affichage de la Map et ajouter une course -->
 	
 	<div class="container">
+		<div class="row mapsCreateRun">
+			<div class="col-md-12 mapsCreateRun text-center">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20992.494806491482!2d2.222156289772821!3d48.87609744963252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x47e67ada5b5ac9db%3A0x4e579c1149864c2e!2sBois+de+Boulogne%2C+Paris!3m2!1d48.862440199999995!2d2.2491727!4m5!1s0x47e664e5248c7707%3A0xa682d0ab0702559d!2s134+Avenue+Pablo+Picasso%2C+Nanterre!3m2!1d48.887978499999996!2d2.2247219!5e0!3m2!1sfr!2sfr!4v1455551657149" 
+				width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>	
 
-		<!-- affichage de la map -->
-		<p>GOOGLE MAP</p>
 
 
 		<hr class="hr-footer">
