03115bdd087399c5efa94b8b7b7525ebf246573e
PaulineS <po_se_87@hotmail.com>
2016-02-18 16:59:40 +0100

    modif taille map + position hr

--- app/templates/run/createrun.php
+++ app/templates/run/createrun.php
@@ -82,8 +82,9 @@
 	<div class="container">
 		<div class="row mapsCreateRun">
 			<div class="col-md-12 mapsCreateRun text-center">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20992.494806491482!2d2.222156289772821!3d48.87609744963252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x47e67ada5b5ac9db%3A0x4e579c1149864c2e!2sBois+de+Boulogne%2C+Paris!3m2!1d48.862440199999995!2d2.2491727!4m5!1s0x47e664e5248c7707%3A0xa682d0ab0702559d!2s134+Avenue+Pablo+Picasso%2C+Nanterre!3m2!1d48.887978499999996!2d2.2247219!5e0!3m2!1sfr!2sfr!4v1455551657149" 
-				width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<p>Valider le point de départ</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.421280120741!2d2.2253670160198156!3d48.88830797929036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e664e4cef67dc9%3A0xab3a6e574aaaafbb!2s136+Avenue+Pablo+Picasso%2C+92000+Nanterre!5e0!3m2!1sfr!2sfr!4v1455810481572" 
+						width="700" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 		</div>
 
