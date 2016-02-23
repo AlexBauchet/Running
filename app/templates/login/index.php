cd6fda69234a079a1c5c5949b0014d0a8c3b5493
PaulineS <po_se_87@hotmail.com>
2016-02-23 15:32:30 +0100

    design box login & register

--- app/templates/login/index.php
+++ app/templates/login/index.php
@@ -12,48 +12,51 @@
 					</div>
 				<?php endif; ?>
 
-				<form class="form-container formLoginRegister" method="POST" action="<?= $this->url('loginUser'); ?>">
+				<div class="boxLoginRegister">
 
-					<!-- Bouttons Connexion Facebook/Google+ -->
-					<div class="button">
-						<button type="button" class="btn btn-fb" data-dismiss="modal"><i id="iFb" class="fa fa-facebook-official fa-2x"></i>Connexion avec Facebook</button>
-					</div>
+					<form class="form-container formLoginRegister" method="POST" action="<?= $this->url('loginUser'); ?>">
 
-					<div class="button">
-						<button type="button" class="btn btn-google" data-dismiss="modal"><i id="iGoogle" class="fa fa-google fa-2x"></i>Connexion avec Google+</button>
-					</div>
+						<!-- Bouttons Connexion Facebook/Google+ -->
+						<div class="button">
+							<button type="button" class="btn btn-fb" data-dismiss="modal"><i id="iFb" class="fa fa-facebook-official fa-2x"></i>Connexion avec Facebook</button>
+						</div>
 
-					<div class="text-center ouEmail">
-						<p>- ou connectez-vous avec votre email -</p>
-					</div>
+						<div class="button">
+							<button type="button" class="btn btn-google" data-dismiss="modal"><i id="iGoogle" class="fa fa-google fa-2x"></i>Connexion avec Google+</button>
+						</div>
 
-					<div class="form-group">
-						<label for="email" hidden>Email</label>
-						<input type="email" class="form-control" id="email" name="email" placeholder="Votre email">
-					</div>
+						<div class="text-center ouEmail">
+							<p>- ou connectez-vous avec votre email -</p>
+						</div>
 
-					<div class="form-group">
-						<label for="password" hidden>Password</label>
-						<input type="password" class="form-control" id="password" name="password" placeholder="Votre mot de passe">
-					</div>
+						<div class="form-group">
+							<label for="email" hidden>Email</label>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Votre email">
+						</div>
 
-					<div>
-						<label id="labCgu" for="cgu" class="pure-checkbox">
-							<input id="memoId" name="memoId" type="checkbox"> Se souvenir de moi</a>
-						</label>
-						<p id="forgetPwd"><a class="forgetPwd" href="forgetPwd.php">Mot de passe oublié ?</a></p>
-					</div>	
+						<div class="form-group">
+							<label for="password" hidden>Password</label>
+							<input type="password" class="form-control" id="password" name="password" placeholder="Votre mot de passe">
+						</div>
 
-					<div class="btn-form btnModal">
-						<button type="submit" class="btn btn-primary">Connexion</button>
-					</div>
+						<div>
+							<label id="labCgu" for="cgu" class="pure-checkbox">
+								<input id="memoId" name="memoId" type="checkbox"> Se souvenir de moi</a>
+							</label>
+							<p id="forgetPwd"><a class="forgetPwd" href="forgetPwd.php">Mot de passe oublié ?</a></p>
+						</div>	
 
-					<div class="modal-footer">
-						<p>Pas encore membre Running ? <a href="<?= $this->url('register') ?>">Inscription</a></p>
-					</div>
-	
-				</form>
+						<div class="btn-form btnModal">
+							<button type="submit" class="btn btn-primary">Connexion</button>
+						</div>
 
+						<div class="modal-footer">
+							<p>Pas encore membre Running ? <a href="<?= $this->url('register') ?>">Inscription</a></p>
+						</div>
+		
+					</form>
+					
+				</div>
 			</div>
 		</div>
 
--- app/templates/register/index.php
+++ app/templates/register/index.php
@@ -12,54 +12,57 @@
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
 
-				<!-- <form class="form-container> -->
-				<form class="form-container formLoginRegister" method="POST" action="<?= $this->url('registerUser'); ?>">
+				<div class="boxLoginRegister">
 
-					<!-- Bouttons Connexion Facebook/Google+ -->
-					<div class="button">
-						<button type="button" class="btn btn-fb" data-dismiss="modal"><i id="iFb" class="fa fa-facebook-official fa-2x"></i>Inscription avec Facebook</button>
-					</div>
+					<form class="form-container formLoginRegister" method="POST" action="<?= $this->url('registerUser'); ?>">
 
-					<div class="button">
-						<button type="button" class="btn btn-google" data-dismiss="modal"><i id="iGoogle" class="fa fa-google fa-2x"></i>Inscription avec Google+</button>
-					</div>
+						<!-- Bouttons Connexion Facebook/Google+ -->
+						<div class="button">
+							<button type="button" class="btn btn-fb" data-dismiss="modal"><i id="iFb" class="fa fa-facebook-official fa-2x"></i>Inscription avec Facebook</button>
+						</div>
 
-					<div class="text-center ouEmail">
-						<p>- ou inscrivez-vous avec votre email -</p>
-					</div>
+						<div class="button">
+							<button type="button" class="btn btn-google" data-dismiss="modal"><i id="iGoogle" class="fa fa-google fa-2x"></i>Inscription avec Google+</button>
+						</div>
 
-					<!-- Email -->
-					<div class="form-group <?php if(isset($errors['email'])) echo 'has-error' ?>">				
-						<input type="email" class="form-control" id="email" name="email" value="<?php if(isset($email)) echo $email; ?>" placeholder="Votre email">
-						<?php if(isset($errors['email'])): ?>
-							<span class="help-block"><?= $errors['email']; ?></span>
-						<?php endif; ?>
-					</div>
+						<div class="text-center ouEmail">
+							<p>- ou inscrivez-vous avec votre email -</p>
+						</div>
 
-					<!-- Mot de passe -->
-					<div class="form-group <?php if(isset($errors['password'])) echo 'has-error' ?>">				
-						<input type="password" class="form-control" id="password" name="password" placeholder="Votre mot de passe">
-						<?php if(isset($errors['password'])): ?>
-							<span class="help-block"><?= $errors['password']; ?></span>
-						<?php endif; ?>
-					</div>
+						<!-- Email -->
+						<div class="form-group <?php if(isset($errors['email'])) echo 'has-error' ?>">				
+							<input type="email" class="form-control" id="email" name="email" value="<?php if(isset($email)) echo $email; ?>" placeholder="Votre email">
+							<?php if(isset($errors['email'])): ?>
+								<span class="help-block"><?= $errors['email']; ?></span>
+							<?php endif; ?>
+						</div>
 
-					<!-- Confirm Mdp -->
-					<div class="form-group">					
-						<input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" placeholder="Confirmer votre mot de passe">
-					</div>
+						<!-- Mot de passe -->
+						<div class="form-group <?php if(isset($errors['password'])) echo 'has-error' ?>">				
+							<input type="password" class="form-control" id="password" name="password" placeholder="Votre mot de passe">
+							<?php if(isset($errors['password'])): ?>
+								<span class="help-block"><?= $errors['password']; ?></span>
+							<?php endif; ?>
+						</div>
 
-					<p class="cgu">En créant un compte, vous acceptez les <a>Conditions générales</a> et la <a>Politique de confidentialité</a>.</p>
+						<!-- Confirm Mdp -->
+						<div class="form-group">					
+							<input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" placeholder="Confirmer votre mot de passe">
+						</div>
 
-					<div class="btn-form btnModal">
-						<button type="submit" class="btn btn-primary">Inscription</button>
-					</div>
+						<p class="cgu">En créant un compte, vous acceptez les <a>Conditions générales</a> et la <a>Politique de confidentialité</a>.</p>
 
-					<div class="modal-footer">
-						<p>Déjà membre Running ? <a href="<?= $this->url('login') ?>">Connexion</a></p>
-					</div>
+						<div class="btn-form btnModal">
+							<button type="submit" class="btn btn-primary">Inscription</button>
+						</div>
 
-				</form>
+						<div class="modal-footer">
+							<p>Déjà membre Running ? <a href="<?= $this->url('login') ?>">Connexion</a></p>
+						</div>
+
+					</form>
+
+				</div>	
 
 			</div>
 			
